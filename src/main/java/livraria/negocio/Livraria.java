5b9576d3eacecae01dc685a8399dd2eade323abd
Tiago Gandra <tiagogandrati@gmail.com>
2021-11-19 23:19:49 -0300

    add class livraria, add class carrinhocompras

---
+++ src/main/java/livraria/negocio/Livraria.java
@@ -0,0 +1,146 @@
+package livraria.negocio;
+
+import java.util.ArrayList;
+
+import java.util.Collection;
+
+import java.util.Collections;
+
+import java.util.Iterator;
+
+import java.util.List;
+
+import livraria.negocio.excecoes.CompraException;
+
+import livraria.negocio.excecoes.LivroNaoEncontradoException;
+
+
+public class Livraria {
+
+    private List<Livro> estoqueLivros;
+
+    public Livraria(){
+
+        estoqueLivros = new ArrayList<Livro>();
+
+        popularLivros();
+
+    }
+
+    private void popularLivros(){
+
+        Livro livro = new Livro();
+
+        livro.setIdLivro("0596005407");
+
+        livro.setAno(2008);
+
+        livro.setTitulo("Head First Servlets and JSP");
+
+        livro.setDescricao("Livro sobre Servlets e JSP");
+
+        livro.setAutores("Bryan Basham, Kathy Sierra, Bert Bates");
+
+        livro.setQuantidade(10);
+
+        livro.setPreco(200.5);
+
+        estoqueLivros.add(livro);
+
+        livro = new Livro();
+
+        livro.setIdLivro("97885573935721");
+
+        livro.setAno(2007);
+
+        livro.setTitulo("Desenvolvendo Aplicações Web com JSP, Servlets,"
+        + " JavaServer Faces, Hibernate, EJB 3 Persistence e Ajax");
+
+        livro.setDescricao(
+                "Livro sobre tecnologias usadas na programação Java pra Web");
+
+        livro.setAutores("Edson Gonçalves");
+
+        livro.setQuantidade(10);
+
+        livro.setPreco(110.9);
+
+        estoqueLivros.add(livro);
+    }
+
+    public List<Livro> getLivros(){
+
+        return Collections.unmodifiableList(estoqueLivros);
+
+    }
+
+    public Livro getLivro(String idLivro) throws LivroNaoEncontradoException{
+
+        Livro livroProcurado = null;
+
+        for(Livro book : estoqueLivros){
+
+            if(book.getIdLivro().equals(idLivro)){
+
+                livroProcurado = book;
+
+            }
+
+            if (livroProcurado == null){
+
+                throw new LivroNaoEncontradoException(
+                        "Não foi possivel encontrar o livro: " + idLivro);
+            }
+        }
+
+        return livroProcurado;
+    }
+
+    public void comprarLivros(CarrinhoCompras carrinho) throws CompraException{
+
+        Collection<ItemCompra> items = carrinho.getItens();
+
+        Iterator<ItemCompra> i = items.iterator();
+
+        while(i.hasNext()){
+
+            ItemCompra item = (ItemCompra) i.next();
+
+            Livro livro = (Livro) item.getItem();
+
+            String id = livro.getIdLivro();
+
+            int quantity = item.getQuantidade();
+
+            comprarLivro(id, quantity);
+
+        }
+    }
+
+    public void comprarLivro(String idLivro, int qtdComprada) throws CompraException {
+
+        Livro livroSelecionado;
+
+        try{
+
+            LivroSelecionado = getLivro(idLivro);
+
+        }
+        catch (LivroNaoEncontradoException e){
+
+            throw new CompraException(e.getMessage());
+        }
+
+        int qtdEstoque = livroSelecionado.getQuantidade();
+
+        if((qtdEstoque - qtdComprada) >= 0) {
+
+            int novaQtd = qtdEstoque - qtdComprada;
+
+            livroSelecionado.setQuantidade(novaQtd);
+        }
+        else{
+            throw new CompraException("Livro" + idLivro + "sem estoque suficiente.");
+        }
+    }
+}
